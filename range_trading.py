e6f2e2666d86f6609a9183d86f559ebe55c8a2f6
Lucedoke <lucas.sicili@gmail.com>
2022-09-22 13:27:49 +0800

    Added RSI and Toggle for Remaking data

--- range_trading.py
+++ range_trading.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
-import time
-import multiprocessing as mp
 
 # local imports
-from backtester import engine, tester
-from backtester import API_Interface as api
+from backtester import  tester
 
-training_period = 20 # How far the rolling average takes into calculation
-standard_deviations = 3.5 # Number of Standard Deviations from the mean the Bollinger Bands sit
 
-interval = 14
+TRAINING_PERIOD = 20 # How far the rolling average takes into calculation
+STD_DV = 3.5 # Number of Standard Deviations from the mean the Bollinger Bands sit
+
+INTERVAL = 14
+
+RSI_BOUNDS = {
+    "upper": 70,
+    "lower": 30,
+}
 
 '''
 logic() function:
@@ -23,8 +26,6 @@ logic() function:
 '''
 
 
-
-
     # lookback is going to be a pandas dataframe e.g. 'Open':[100, 87, 69, 11]
                                                     # 'Volume':[23, 45, 76, 93]
     
@@ -32,9 +33,11 @@ def logic(account, lookback): # Logic function to be used for each time interval
 
     last_index = len(lookback)-1
 
-    if(last_index <= training_period): # If the lookback is long enough to calculate the Bollinger Bands
+    if(last_index <= TRAINING_PERIOD): # If the lookback is long enough to calculate the Bollinger Bands
         return   # A coding style that is the same as tabbed if statement.
-    ADX = lookback['ADX'+str(interval)][last_index]
+    ADX = lookback['ADX'+str(INTERVAL)][last_index]
+
+    RSI = lookback['RSI'][last_index]
 
     # when ADX is >= 20, we don't trade
     if ADX >= 20:
@@ -43,12 +46,22 @@ def logic(account, lookback): # Logic function to be used for each time interval
     # when ADX is < 20, its sideways market, use boll bands strat
     close_price = lookback['close'][last_index]
     if(close_price < lookback['BOLD'][last_index]): # If current price is below lower Bollinger Band, enter a long position
+        
+        # if RSI > 70 - don't buy (Overvalued)
+        if RSI > RSI_BOUNDS["upper"]:
+            return
+
         for position in account.positions: # Close all current positions
             account.close_position(position, 1, close_price)
         if(account.buying_power > 0):
             account.enter_position('long', account.buying_power, close_price) # Enter a long position (enter full position)
 
     if(close_price > lookback['BOLU'][last_index]): # If today's price is above the upper Bollinger Band, enter a short position
+        
+        # if RSI < 30 - don't sell (Undervalued)
+        if RSI < RSI_BOUNDS["lower"]:
+            return
+        
         for position in account.positions: # Close all current positions
             account.close_position(position, 1, close_price)
         if(account.buying_power > 0):
@@ -70,6 +83,21 @@ def logic(account, lookback): # Logic function to be used for each time interval
     #         if(account.buying_power > 0):
     #             account.enter_position('short', account.buying_power, lookback['close'][today]) # Enter a short position
 
+def calc_rsi(data, periods=14):
+    close_delta = data['close'].diff()
+
+    # Make two series: one for lower closes and one for higher closes
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+
+    ma_up = up.ewm(com=periods - 1, adjust=True, min_periods=periods).mean()
+    ma_down = down.ewm(com=periods - 1, adjust=True,
+                       min_periods=periods).mean()
+
+    rsi = ma_up / ma_down
+    rsi = 100 - (100/(1 + rsi))
+    return rsi
+
 '''
 preprocess_data() function:
     Context: Called once at the beginning of the backtest. TOTALLY OPTIONAL. 
@@ -85,10 +113,10 @@ def preprocess_data(list_of_stocks):
     for stock in list_of_stocks:
         df = pd.read_csv("data/" + stock + ".csv", parse_dates=[0])
         df['TP'] = (df['close'] + df['low'] + df['high'])/3 # Calculate Typical Price
-        df['std'] = df['TP'].rolling(training_period).std() # Calculate Standard Deviation
-        df['MA-TP'] = df['TP'].rolling(training_period).mean() # Calculate Moving Average of Typical Price
-        df['BOLU'] = df['MA-TP'] + standard_deviations*df['std'] # Calculate Upper Bollinger Band
-        df['BOLD'] = df['MA-TP'] - standard_deviations*df['std'] # Calculate Lower Bollinger Band
+        df['std'] = df['TP'].rolling(TRAINING_PERIOD).std() # Calculate Standard Deviation
+        df['MA-TP'] = df['TP'].rolling(TRAINING_PERIOD).mean() # Calculate Moving Average of Typical Price
+        df['BOLU'] = df['MA-TP'] + STD_DV*df['std'] # Calculate Upper Bollinger Band
+        df['BOLD'] = df['MA-TP'] - STD_DV*df['std'] # Calculate Lower Bollinger Band
         df['sum']=df['close'].cumsum()
 
         df['-DM'] = df['low'].shift(1) - df['low'] # Directional Movement : previous low minus current low
@@ -99,37 +127,50 @@ def preprocess_data(list_of_stocks):
         df['TR_TMP2'] = np.abs(df['high'] - df['close'].shift(1))
         df['TR_TMP3'] = np.abs(df['low'] - df['close'].shift(1))
         df['TR'] = df[['TR_TMP1', 'TR_TMP2', 'TR_TMP3']].max(axis=1)
-        df['TR'+str(interval)] = df['TR'].rolling(interval).sum()
-        df['+DMI'+str(interval)] = df['+DM'].rolling(interval).sum()
-        df['-DMI'+str(interval)] = df['-DM'].rolling(interval).sum()
-        df['+DI'+str(interval)] = df['+DMI'+str(interval)] /   df['TR'+str(interval)]*100
-        df['-DI'+str(interval)] = df['-DMI'+str(interval)] / df['TR'+str(interval)]*100
-        df['DI'+str(interval)+'-'] = abs(df['+DI'+str(interval)] - df['-DI'+str(interval)])
-        df['DI'+str(interval)] = df['+DI'+str(interval)] + df['-DI'+str(interval)]
-        df['DX'] = (df['DI'+str(interval)+'-'] / df['DI'+str(interval)])*100
-        df['ADX'+str(interval)] = df['DX'].rolling(interval).mean()
+        df['TR'+str(INTERVAL)] = df['TR'].rolling(INTERVAL).sum()
+        df['+DMI'+str(INTERVAL)] = df['+DM'].rolling(INTERVAL).sum()
+        df['-DMI'+str(INTERVAL)] = df['-DM'].rolling(INTERVAL).sum()
+        df['+DI'+str(INTERVAL)] = df['+DMI'+str(INTERVAL)] /   df['TR'+str(INTERVAL)]*100
+        df['-DI'+str(INTERVAL)] = df['-DMI'+str(INTERVAL)] / df['TR'+str(INTERVAL)]*100
+        df['DI'+str(INTERVAL)+'-'] = abs(df['+DI'+str(INTERVAL)] - df['-DI'+str(INTERVAL)])
+        df['DI'+str(INTERVAL)] = df['+DI'+str(INTERVAL)] + df['-DI'+str(INTERVAL)]
+        df['DX'] = (df['DI'+str(INTERVAL)+'-'] / df['DI'+str(INTERVAL)])*100
+        df['ADX'+str(INTERVAL)] = df['DX'].rolling(INTERVAL).mean()
 
         
-        df['ADX'+str(interval)] =  df['ADX'+str(interval)].fillna(df['ADX'+str(interval)].mean()) 
-        del df['TR_TMP1'], df['TR_TMP2'], df['TR_TMP3'], df['TR'], df['TR'+str(interval)]
-        del df['+DMI'+str(interval)], df['DI'+str(interval)+'-']
-        del df['DI'+str(interval)], df['-DMI'+str(interval)]       # interval is 14 (ADX14)
-        del df['+DI'+str(interval)], df['-DI'+str(interval)]
+        df['ADX'+str(INTERVAL)] =  df['ADX'+str(INTERVAL)].fillna(df['ADX'+str(INTERVAL)].mean()) 
+        del df['TR_TMP1'], df['TR_TMP2'], df['TR_TMP3'], df['TR'], df['TR'+str(INTERVAL)]
+        del df['+DMI'+str(INTERVAL)], df['DI'+str(INTERVAL)+'-']
+        del df['DI'+str(INTERVAL)], df['-DMI'+str(INTERVAL)]       # interval is 14 (ADX14)
+        del df['+DI'+str(INTERVAL)], df['-DI'+str(INTERVAL)]
         del df['DX']
+
+        # RSI
+        df["RSI"] = calc_rsi(df, INTERVAL)
         df.to_csv("data/" + stock + "_Processed.csv", index=False) # Save to CSV
+
+
         list_of_stocks_processed.append(stock + "_Processed")
     return list_of_stocks_processed
 
 
 if __name__ == "__main__":
-    list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min"] 
-    # list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min", "AAPL_2020-03-24_2022-02-12_15min"] # List of stock data csv's to be tested, located in "data/" folder 
-    list_of_stocks_proccessed = preprocess_data(list_of_stocks) # Preprocess the data
-    # list_of_stocks_proccessed = ["TSLA_2020-03-01_2022-01-20_1min_Processed"]
-    results = tester.test_array(list_of_stocks_proccessed, logic, chart=True) # Run backtest on list of stocks using the logic function
+    
+    REMAKE_DATA = True
+    # remake *_Processed.csv for all stocks in "stocks_to_process"
+    if REMAKE_DATA:
+        stocks_to_process = ["TSLA_2020-03-09_2022-01-28_15min", "AAPL_2020-03-24_2022-02-12_15min"]
+        list_of_stocks = preprocess_data(
+            stocks_to_process)  # Preprocess the data
+    # resuse cvs from "list_of_stocks"
+    else:
+        # List of stock data csv's to be tested, located in "data/" folder
+        #"TSLA_2020-03-09_2022-01-28_15min_Processed"
+        list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min_Processed", "AAPL_2020-03-24_2022-02-12_15min_Processed"]
+    results = tester.test_array(list_of_stocks, logic, chart=True) # Run backtest on list of stocks using the logic function
 # passing logic function as parameter. 
-    print("training period " + str(training_period))
-    print("standard deviations " + str(standard_deviations))
+    print("training period " + str(TRAINING_PERIOD))
+    print("standard deviations " + str(STD_DV))
     df = pd.DataFrame(list(results), columns=["Buy and Hold","Strategy","Longs","Sells","Shorts","Covers","Stdev_Strategy","Stdev_Hold","Stock"]) # Create dataframe of results
     df.to_csv("results/Test_Results.csv", index=False) # Save results to csv
 
